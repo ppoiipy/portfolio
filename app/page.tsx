9e409cc525f392d03f4f97edde18b78b4d2175b9
Natthakorn Tainkrue <phobpynatthakorn@gmail.com>
2026-08-08 15:54:27 +0700

    feat: add Badge component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
+import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 
 export default function Home() {
   return (
     <>
-      <div className="p-8 flex gap-4">
-        <Button>Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="danger" size="sm">Danger small</Button>
-        <Button disabled>Disabled</Button>
-      </div>
-      <div className="pl-8">
-        <Card className="mt-8 max-w-sm">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-        </Card>
+      <div className="p-8">
+        <div className="flex gap-4">
+          <Button>Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="danger" size="sm">Danger small</Button>
+          <Button disabled>Disabled</Button>
+        </div>
+          <Card className="mt-8 max-w-sm">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+          </Card>
+        <div className="mt-8 flex gap-2">
+          <Badge variant="default">Default</Badge>
+          <Badge variant="outline">Outline</Badge>
+          <Badge variant="accent">Accent</Badge>
+        </div>
       </div>
     </>
   );
---
+++ components/ui/Badge/index.tsx
@@ -0,0 +1,36 @@
+import { HTMLAttributes, forwardRef } from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+    "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium transition-colors",
+    {
+        variants: {
+            variant: {
+                default: "border-gray-700 bg-gray-800 text-gray-300",
+                outline: "border-gray-700 text-gray-300",
+                accent: "border-transparent bg-white text-black"
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+)
+
+export interface BadgeProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> { }
+
+const Badge = forwardRef<HTMLDivElement, BadgeProps>(
+    ({ className, variant, ...props }, ref) => {
+        return (
+            <div 
+                ref={ref}
+                className={badgeVariants({ variant, className })} 
+                {...props}
+            />
+        )
+    }
+);
+Badge.displayName = "Badge";
+
+export { Badge, badgeVariants }
