b4a0242932701e59896fcd8ef34fa1e3a8edb548
Natthakorn Tainkrue <phobpynatthakorn@gmail.com>
2026-08-21 23:12:06 +0700

    fix: change footer to stay at bottom on short page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        {children}
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
