4554a81a9f6b252459fea5864d3232466860c2e5
Natthakorn Tainkrue <phobpynatthakorn@gmail.com>
2026-08-22 16:42:32 +0700

    feat: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navbar />
         <div className="flex-1">{children}</div>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
